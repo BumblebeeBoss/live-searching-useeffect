0c98a6111afeb36e69fb8b96fd16df311fe7faa2
BumblebeeBoss <suhailanas150@gmail.com>
2023-12-18 07:22:34 -0800

    live-searching-useeffect

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import Basics from "./Components/Basics";
+import LiveSearching from "./components/LiveSearching"
+
+const App = () => {
+
+
+  return(
+    <div>
+      {/* <Basics /> */}
+      <LiveSearching />
+    </div>
+  )
+}
+
+export default App;
